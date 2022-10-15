6932d9fdd896c6081e8ae2dfe19f00f068a460bf
Albert Sun <albertsun0@gmail.com>
2022-10-15 16:49:20 -0500

    908iou

--- frontend/App.js
+++ frontend/App.js
@@ -2,14 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import tw from 'twrnc';
 import { useDeviceContext } from 'twrnc';
+import SignIn from './screens/auth/SignIn';
 
 export default function App() {
   useDeviceContext(tw);
   return (
-    <View style={tw`h-full flex bg-white items-center justify-center`}>
-      <StatusBar style="auto" />
-      <Text style={tw`text-xl`}>Activities App</Text>
-    </View>
+    <SignIn></SignIn>
   );
 }
 
